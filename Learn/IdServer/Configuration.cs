b125db9e57d94a1b6c613202a91786e8d9c20243
Yaroslav Fur <tapocki20@gmail.com>
2022-06-11 20:47:00 +0300

    Learn: created authen/author without cookie, only JWT. DB using, fine API and polishing - what's left

--- Learn/IdServer/Configuration.cs
+++ Learn/IdServer/Configuration.cs
@@ -43,7 +43,7 @@ namespace IdServer
                         IdentityServerConstants.StandardScopes.Profile
                     },
 
-                    RequireConsent = false
+                    RequireConsent = false,
                 }
             };
     }
